375e6637aa7bc15510d08007928294c9f7b9ff1e
Georges Ryan <gryan.ab@gmail.com>
2019-03-15 19:42:05 +0100

    adds easy and hard mode
    - easy : returns 3 displays, rearrange color attribution.
    - hard: returns 6 displays
    - adds 'selected' class depending on mode playing.

--- colorgame.js
+++ colorgame.js
@@ -1,15 +1,48 @@
-var colors = generateRandomColors(6);
+var numberOfSquares = 6;
+var colors = generateRandomColors(numberOfSquares);
 var squares = document.querySelectorAll('.square');
 var colorDisplay = document.getElementById('colorDisplay');
 var pickedColor = pickColor();
 var messageDisplay = document.querySelector('#message');
 var h1 = document.querySelector('h1');
 var resetButton = document.querySelector('#reset');
+var easyBtn = document.querySelector('#easy-btn');
+var hardBtn = document.querySelector('#hard-btn');
+
+easyBtn.addEventListener('click', function(){
+  easyBtn.classList.add('selected');
+  hardBtn.classList.remove('selected');
+  numberOfSquares = 3;
+  colors = generateRandomColors(numberOfSquares);
+  pickedColor = pickColor();
+  colorDisplay.textContent = pickedColor;
+  for (var i = 0; i < squares.length; i++) {
+    if(colors[i]){
+      squares[i].style.backgroundColor = colors[i];
+    } else {
+      squares[i].style.display = 'none';
+    }
+  };
+});
+
+hardBtn.addEventListener('click', function(){
+  hardBtn.classList.add('selected');
+  easyBtn.classList.remove('selected');
+  numberOfSquares = 6;
+  colors = generateRandomColors(numberOfSquares);
+  pickedColor = pickColor();
+  colorDisplay.textContent = pickedColor;
+  for (var i = 0; i < squares.length; i++) {
+      squares[i].style.backgroundColor = colors[i];
+      squares[i].style.display = 'block';
+  }
+});
 
 resetButton.addEventListener('click', function(){
   h1.style.backgroundColor = '#232323';
+  resetButton.textContent = 'New Colors';
   // generate all new Colors
-  colors = generateRandomColors(6);
+  colors = generateRandomColors(numberOfSquares);
   // pick a new random color from array
   pickedColor = pickColor()
   colorDisplay.textContent = pickedColor;
