a7332702f3f50d960e2be76723ca660d103c55d8
Georges Ryan <gryan.ab@gmail.com>
2019-03-15 18:58:40 +0100

    adding real random color logic

--- colorgame.js
+++ colorgame.js
@@ -1,11 +1,4 @@
-var colors = [
-  'rgb(255, 0, 0)',
-  'rgb(255, 255, 255)',
-  'rgb(255, 255, 0)',
-  'rgb(255, 0, 255)',
-  'rgb(0, 255, 0)',
-  'rgb(0, 255, 255)',
-];
+var colors = generateRandomColors(6);
 var squares = document.querySelectorAll('.square');
 var colorDisplay = document.getElementById('colorDisplay');
 var pickedColor = pickColor();
@@ -39,4 +32,21 @@ function changeColors(color){
 function pickColor(){
   var random = Math.floor(Math.random() * colors.length);
   return colors[random];
-}
+};
+
+function generateRandomColors(n){
+  var array = [];
+  for (var i = 0; i < n; i++) {
+    // get random color and push into array
+    array.push(randomColor());
+  };
+  return array;
+};
+
+function randomColor(){
+  // pick a 'red', 'green', 'blue' from 0 to 255
+  var r = Math.floor(Math.random() * 256);
+  var g = Math.floor(Math.random() * 256);
+  var b = Math.floor(Math.random() * 256);
+  return 'rgb('+ r + ', ' + g + ', ' + b +')';
+};
